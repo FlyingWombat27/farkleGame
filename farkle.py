08f295fa9b4cae5766bb744d49913ca8a0c0ab3b
FlyingWombat27 <FlyingWombat109@gmail.com>
2020-11-29 12:22:21 -0600

    start rolling logic

--- farkle.py
+++ farkle.py
@@ -2,27 +2,34 @@ import random
 import numpy as np
 
 def main():
-    team_name1 = str(input('What is the name of team one?'))
-    team_name2 = str(input('What is the name of team two?'))
-    who_goes_first = str(input('Who will go first?'))
+    player_name1 = str(input('What is the name of player one? '))
+    player_name2 = str(input('What is the name of player two? '))
     dice_rolls = 6
     dice_size = 6
-    who_goes_second = str()
     dice_avail = []
-    
-    if who_goes_first == team_name1:
-        who_goes_second = team_name2
-    else:
-        who_goes_second = team_name1
+    dice_kept = []
         
-    print(f'{who_goes_second} goes second!')
-    
     for i in range(0,dice_rolls):
         roll = random.randint(1,dice_size)
         dice_avail.append(roll)
+    
     dice_avail = np.array(dice_avail)    
     dice_avail = np.sort(dice_avail)
-    print(f'{who_goes_first}, you rolled the following: {dice_avail} ')
+    
+    print(f'{player_name1}, you rolled the following: {dice_avail} ')
+    num_dice_kept = int(input(f'{player_name1}, how many dice would you like to keep? '))   
+    
+    for i in range(0,num_dice_kept):
+        keep = int(input(f'What is dice {i+1} that you would like to keep? '))            
+        dice_kept.append(keep)
+ 
+    dice_rolls -= keep
+    dice_avail = []
+    
+    for i in range(0, dice_rolls):
+        roll = random.randint(1,dice_size)
+        dice_avail.append(roll)
+        
     
 if __name__== "__main__":
   main()
